6fd77b68798e4e280fc5dc5b7961b6740ede9b94
gellidao <gellidao@gmail.com>
2021-06-18 15:05:13 -0500

    Now the quaternios are being reading and sended via serial, and are correctly recieved in matlab. Next task is to generates the visualization in simscape multibody

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,41 @@
 #include <Arduino.h>
+#include <MPU_personalized_functions.h>
 
-byte a[4] ;
+uint32_t prev = 0;
+byte Quaternion[6] ; 
 
-byte fractionOfTime(void){
-  int time = millis(); 
-  int time_fraction; 
-  time_fraction = time % 1000; 
-  return byte(255*time_fraction/1000) ; 
+byte quat2Byte(float q){
+  return byte(q*255) ; 
+}
+
+void sendQuaternionByte(MPU9250 &mpu){
+  byte Quaternion[6] ;
+  Quaternion[0] = 'R'; 
+  Quaternion[1] = quat2Byte(mpu.getQuaternionX()) ; 
+  Quaternion[2] = quat2Byte(mpu.getQuaternionY()) ; 
+  Quaternion[3] = quat2Byte(mpu.getQuaternionZ()) ;
+  Quaternion[4] = quat2Byte(mpu.getQuaternionW());
+  Quaternion[5] = '\n' ; 
+  //Serial.print(Quaternion[1]); 
+  Serial.write(Quaternion, 6); // 4 es el número de bytes que se envían
 }
 
 void setup() {
   Serial.begin(115200);
+  Wire.begin();
+  inicializeMPU9250(mpu); 
 }
 
 void loop() {
-  a[0] = 'R'; 
-  a[1] = byte(10) ; 
-  a[2] = fractionOfTime() ; 
-  a[3] = '\n' ; 
-  Serial.write(a, 4); // 4 es el número de bytes que se envían
-  delay(10) ; 
+  uint32_t now = micros();
+    if(now - prev >= 10000){ // Send each 10 ms
+      while(true){
+      if (mpu.update()) { // If mpu is ready, continue
+        prev = micros();
+
+        sendQuaternionByte(mpu); // Read and send current quaternion
+        break ; 
+    } //end if
+    } // end while
+    } // end if
 }
